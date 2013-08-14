6721d895a3a80271801bf9cd402961d239a69444
Tuomas Angervuori <fiangeto@fiangeto-dev>
2013-08-14 16:02:44 +0300

    Example how to identify user

---
+++ examples/identifyUser.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * How to identify the current user
+ * 
+ * Authenticates the user (if possible) and displays the username. An exception is thrown if user authentication is not possible.
+ * 
+ * @author Tuomas Angervuori <tuomas.angervuori@gmail.com>
+ */
+
+require_once '../user.php';
+$user = new User();
+$user->loadConfig('config.ini');
+echo $user->identify() . "\n";
